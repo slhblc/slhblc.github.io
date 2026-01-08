ec0b0be2c8c3c3e4b051c98f9e65a8c9f80aae35
slhblc <salih.bolac@hisarschool.k12.tr>
2026-01-08 08:33:16 +0300

    Create listclockproject.swift

---
+++ listclockproject.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    let music = ["Rock","Pop","Metal","Jazz"]
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("My Array Map V1 - Music")
+                .font(.title2)
+                .padding(.bottom, 10)
+        }
+    }
+}
+
